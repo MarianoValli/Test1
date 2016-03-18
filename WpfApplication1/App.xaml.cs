ebb8903e89e9fca0f5a3905d70f73ae6aecc59ed
Mariano Valli <mariano.valli@greiner.it>
2016-03-18 15:58:09 +0100

    Revert "Commit 7"
    This reverts commit 15e4f863ecdbc12d9d99ef97a91cc9532039f4ff.

--- WpfApplication1/App.xaml.cs
+++ WpfApplication1/App.xaml.cs
@@ -1,6 +1,18 @@
-﻿namespace WpfApplication1
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
 {
-    public partial class App
+    /// <summary>
+    /// Logica di interazione per App.xaml
+    /// </summary>
+    public partial class App : Application
     {
+        // commento
     }
 }
